38b85e52fb7d9ccf819591deb1b100fb2f501496
sébastien <sebastien.faure24@orange.fr>
2023-11-03 15:43:53 +0100

    kjjhjhj

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/year', function (req, res) {
   const today = new Date().getFullYear().toString()
-  res.json({ result: today });
+  res.json({ year: today });
 });
 
 module.exports = router;
